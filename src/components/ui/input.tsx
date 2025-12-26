df5480901bafe9e1ccb7181e550b58630a79f7eb
Trey Tallent <treydentallent@gmail.com>
2025-12-26 12:55:12 +0800

    Add input, select & textarea

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,105 @@
+import * as Headless from '@headlessui/react'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export function InputGroup({
+  className,
+  children,
+}: React.ComponentPropsWithoutRef<'span'>) {
+  return (
+    <span
+      data-slot="control"
+      className={twMerge(
+        // Basic layout
+        'relative isolate block',
+        // X-axis padding
+        'has-[[data-slot=icon]:first-child]:[&_input]:pl-10 has-[[data-slot=icon]:last-child]:[&_input]:pr-10 sm:has-[[data-slot=icon]:first-child]:[&_input]:pl-8 sm:has-[[data-slot=icon]:last-child]:[&_input]:pr-8',
+        // Icon style
+        '*:data-[slot=icon]:size-5 *:data-[slot=icon]:text-obsidian-400 sm:*:data-[slot=icon]:size-4',
+        '*:data-[slot=icon]:pointer-events-none *:data-[slot=icon]:absolute *:data-[slot=icon]:z-10',
+        // Icon positions
+        '*:data-[slot=icon]:top-3 sm:*:data-[slot=icon]:top-2.5',
+        '[&>[data-slot=icon]:first-child]:left-3 sm:[&>[data-slot=icon]:first-child]:left-2.5',
+        '[&>[data-slot=icon]:last-child]:right-3 sm:[&>[data-slot=icon]:last-child]:right-2.5',
+        className
+      )}
+    >
+      {children}
+    </span>
+  )
+}
+
+const dateTypes = ['date', 'datetime-local', 'month', 'time', 'week']
+type DateType = (typeof dateTypes)[number]
+
+export function Input({
+  focusRing = true,
+  className,
+  ...props
+}: {
+  focusRing?: boolean
+  className?: string
+  type?:
+    | 'email'
+    | 'number'
+    | 'password'
+    | 'search'
+    | 'tel'
+    | 'text'
+    | 'url'
+    | DateType
+} & Omit<Headless.InputProps, 'as' | 'className'>) {
+  return (
+    <span
+      data-slot="control"
+      className={twMerge([
+        // Basic layout
+        'relative block w-full',
+        // Disabled state
+        'has-data-disabled:opacity-50',
+        // Focus ring
+        focusRing &&
+          'after:pointer-events-none after:absolute after:inset-0 after:ring-transparent after:ring-inset sm:focus-within:after:ring-2 sm:focus-within:after:ring-blue-500',
+        className,
+      ])}
+    >
+      <Headless.Input
+        {...props}
+        className={twMerge(
+          // Basic layout
+          'relative block w-full appearance-none border px-[calc(--spacing(4)-1px)] py-[calc(--spacing(3)-1px)] sm:py-[calc(--spacing(2)-1px)]',
+          // Typography
+          'text-base/5 placeholder:text-obsidian-400 sm:text-sm/5',
+          // Border
+          'border-white/10 data-hover:border-white/20',
+          // Background color
+          'bg-white/5',
+          // Hide default focus styles
+          'focus:outline-hidden',
+          // System icons
+          'scheme-dark',
+          // Invalid state
+          'data-invalid:border-red-500 data-invalid:data-hover:border-red-500 dark:data-invalid:border-red-500 dark:data-invalid:data-hover:border-red-500',
+          // Disabled state
+          'data-disabled:border-transparent data-disabled:opacity-50',
+          // Date classes
+          props.type &&
+            dateTypes.includes(props.type) && [
+              '[&::-webkit-datetime-edit-fields-wrapper]:p-0',
+              '[&::-webkit-date-and-time-value]:min-h-[1.5em]',
+              '[&::-webkit-datetime-edit]:inline-flex',
+              '[&::-webkit-datetime-edit]:p-0',
+              '[&::-webkit-datetime-edit-year-field]:p-0',
+              '[&::-webkit-datetime-edit-month-field]:p-0',
+              '[&::-webkit-datetime-edit-day-field]:p-0',
+              '[&::-webkit-datetime-edit-hour-field]:p-0',
+              '[&::-webkit-datetime-edit-minute-field]:p-0',
+              '[&::-webkit-datetime-edit-second-field]:p-0',
+              '[&::-webkit-datetime-edit-millisecond-field]:p-0',
+              '[&::-webkit-datetime-edit-meridiem-field]:p-0',
+            ]
+        )}
+      />
+    </span>
+  )
+}
---
+++ src/components/ui/select.tsx
@@ -0,0 +1,82 @@
+import * as Headless from '@headlessui/react'
+import React, { forwardRef } from 'react'
+import { twJoin, twMerge } from 'tailwind-merge'
+
+export const Select = forwardRef(function Select(
+  {
+    multiple = false,
+    className,
+    ...props
+  }: { className?: string } & Omit<Headless.SelectProps, 'as' | 'className'>,
+  ref: React.ForwardedRef<HTMLSelectElement>
+) {
+  return (
+    <span
+      data-slot="control"
+      className={twMerge([
+        // Basic layout
+        'group relative block w-full',
+        // Focus ring
+        'after:pointer-events-none after:absolute after:inset-0 after:ring-transparent after:ring-inset has-data-focus:after:ring-2 has-data-focus:after:ring-blue-500',
+        // Override inherited border-radius to avoid scrollbar clipping
+        multiple && 'has-data-focus:after:rounded-r-none!',
+        // Disabled state
+        'has-data-disabled:opacity-50 has-data-disabled:before:bg-obsidian-950/5 has-data-disabled:before:shadow-none',
+        className,
+      ])}
+    >
+      <Headless.Select
+        ref={ref}
+        multiple={multiple}
+        {...props}
+        className={twJoin([
+          // Override inherited border-radius to avoid scrollbar clipping
+          multiple && 'rounded-r-none!',
+          // Basic layout
+          'relative block w-full appearance-none pt-[calc(--spacing(5.5)-1px)] pb-[calc(--spacing(2)-1px)]',
+          // Horizontal padding
+          multiple
+            ? 'scrollbar-thin px-4'
+            : 'pr-[calc(--spacing(10)-1px)] pl-4 sm:pr-[calc(--spacing(9)-1px)]',
+          // Options (multi-select)
+          '[&_optgroup]:font-semibold',
+          // Typography
+          'text-base/5 placeholder:text-obsidian-400 sm:text-sm',
+          // Border
+          'border border-white/10 data-hover:border-white/20',
+          // Background color
+          'bg-white/5',
+          // Hide default focus styles
+          'focus:outline-hidden',
+          // Invalid state
+          'data-invalid:border-red-500 data-invalid:data-hover:border-red-500',
+          // Disabled state
+          'data-disabled:opacity-50',
+        ])}
+      />
+      {!multiple && (
+        <span className="pointer-events-none absolute -top-1.75 right-0 flex items-center pr-2 sm:-top-1">
+          <svg
+            className="size-5 stroke-obsidian-400 group-has-data-disabled:stroke-obsidian-600 sm:size-4 forced-colors:stroke-[CanvasText]"
+            viewBox="0 0 16 16"
+            aria-hidden="true"
+            fill="none"
+          >
+            <path
+              d="M5.75 10.75L8 13L10.25 10.75"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M10.25 5.25L8 3L5.75 5.25"
+              strokeWidth={1.5}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+      )}
+    </span>
+  )
+})
