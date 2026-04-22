3b81207e3f76ef487ac34cb6ccf68db67ff9e81f
Trey Tallent <treydentallent@gmail.com>
2026-04-22 19:14:04 +0800

    Start typed fetch wrapper

--- src/lib/types/utils.ts
+++ src/lib/types/utils.ts
@@ -1,5 +1,22 @@
-import type { operations } from './schema/tmdb-api-schema'
+import type { operations, paths } from './schema/tmdb-api-schema'
+
+/** Obtain the response types of a TMDB operation T. */
+export type TmdbResponses<T extends keyof operations> =
+  operations[T]['responses']
 
 /** Obtain the success response type of a TMDB operation T. */
 export type SuccessResponse<T extends keyof operations> =
   operations[T]['responses'][200]['content']['application/json']
+
+/** HTTP methods included in TMDB's schema. */
+export type TmdbHTTPMethods = 'GET' | 'PUT' | 'POST' | 'DELETE'
+
+/** From an HTTP method T, narrow the possible TMDB paths. */
+export type HTTPMethodPaths<T extends TmdbHTTPMethods> = {
+  [P in keyof paths]: paths[P][Lowercase<T>] extends undefined ? never : P
+}[keyof paths]
+
+/** From a Tmdb schema path operation T, exclude responses from any objects. */
+export type OmitResponses<
+  T extends paths[keyof paths][Lowercase<TmdbHTTPMethods>],
+> = T extends object ? Omit<T, 'responses'> : T
