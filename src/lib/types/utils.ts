6e2b800eb8bb5bdd9b3d54a7db8e0eee28c24e6d
Trey Tallent <treydentallent@gmail.com>
2026-04-25 16:13:58 +0800

    Fetch wrapper finalise parameter generic types

--- src/lib/types/utils.ts
+++ src/lib/types/utils.ts
@@ -1,5 +1,8 @@
 import type { operations, paths } from './schema/tmdb-api-schema'
 
+/** Flatten out the display of properties in generic T. @see https://effectivetypescript.com/2022/02/25/gentips-4-display/ */
+export type Resolve<T> = T extends Function ? T : { [K in keyof T]: T[K] } // eslint-disable-line @typescript-eslint/no-unsafe-function-type
+
 /** Obtain the response types of a TMDB operation T. */
 export type TmdbResponses<T extends keyof operations> =
   operations[T]['responses']
@@ -16,7 +19,20 @@ export type HTTPMethodPaths<T extends TmdbHTTPMethods> = {
   [P in keyof paths]: paths[P][Lowercase<T>] extends undefined ? never : P
 }[keyof paths]
 
-/** From a Tmdb schema path operation T, exclude responses from any objects. */
-export type OmitResponses<
-  T extends paths[keyof paths][Lowercase<TmdbHTTPMethods>],
-> = T extends object ? Omit<T, 'responses'> : T
+/** Omit the responses property of a Tmdb schema path operation. */
+export type OmitResponses<T> = Resolve<
+  T extends paths[keyof paths][Lowercase<TmdbHTTPMethods>]
+    ? T extends object
+      ? Omit<T, 'responses'>
+      : T
+    : never
+>
+
+/** Omits properties whose set is a subset of undefined. A conditional check follows a constraint on T so tooltips display the recursion evaluation. */
+export type OmitUndefinedSubsets<T> = T extends object
+  ? {
+      [P in keyof T as T[P] extends undefined ? never : P]: T[P] extends object
+        ? OmitUndefinedSubsets<T[P]>
+        : T[P]
+    }
+  : T
