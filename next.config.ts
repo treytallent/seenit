6f9553544a38fedcc583e0687c5a72a8a29bf70f
Trey Tallent <treydentallent@gmail.com>
2026-06-11 18:47:40 +0800

    Add turbopack root

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.resolve(dirname),
+  },
 }
 
 export default nextConfig
