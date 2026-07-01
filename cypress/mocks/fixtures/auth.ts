9a3dba50a4c18f485e8ffb6c07108bd89c90e9d9
Trey Tallent <treydentallent@gmail.com>
2026-07-01 15:18:11 +0800

    Remove cypress

--- cypress/mocks/fixtures/auth.ts
+++
@@ -1,9 +0,0 @@
-import type { EndpointSuccessResponse } from '@/lib/types/api'
-
-export const fetchNewAuthenticationToken: EndpointSuccessResponse<
-  'GET',
-  '/3/authentication/token/new'
-> = {
-  success: true,
-  request_token: 'abc123',
-}
--- cypress/mocks/handlers.ts
+++
@@ -1,9 +0,0 @@
-import { TMDB_API_BASE_URL } from '@/lib/constants'
-import { http, HttpResponse } from 'msw'
-import { fetchNewAuthenticationToken } from './fixtures/auth'
-
-export const handlers = [
-  http.get(TMDB_API_BASE_URL.concat('/3/authentication/token/new'), () =>
-    HttpResponse.json(fetchNewAuthenticationToken)
-  ),
-]
