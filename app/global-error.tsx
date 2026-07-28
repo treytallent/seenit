4c28767639323de3c01b6121ac1abbed2f94693f
Trey Tallent <treydentallent@gmail.com>
2026-07-28 19:08:39 +0800

    Add root error boundary captureException & boundary tags

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -10,7 +10,9 @@ export default function GlobalError({
   error: Error & { digest?: string }
 }) {
   useEffect(() => {
-    Sentry.captureException(error)
+    Sentry.captureException(error, {
+      tags: { boundary: 'global-error' },
+    })
   }, [error])
 
   return (
