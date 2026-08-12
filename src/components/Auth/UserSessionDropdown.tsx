89e0fcce0cbabf6068eb2c206e13c57bf9d059ad
Trey Tallent <treydentallent@gmail.com>
2026-08-12 16:16:19 +0800

    Remove placeholder DropdownItem

--- src/components/Auth/UserSessionDropdown.tsx
+++ src/components/Auth/UserSessionDropdown.tsx
@@ -10,7 +10,6 @@ import {
 import { type UserSession } from '@/src/lib/auth/get-session'
 import { deleteUserSession } from '@/src/lib/auth/user-session'
 import * as Headless from '@headlessui/react'
-import { UserIcon } from 'lucide-react'
 import { startTransition, useActionState } from 'react'
 import { NavigationAuthButton } from '../Navigation/NavigationAuth'
 import { LoadingSpinner } from '../ui/icons/loading-spinner'
@@ -36,29 +35,23 @@ function UserDropdown() {
   const [_, action, pending] = useActionState(deleteUserSession, null)
 
   return (
-    <>
-      <DropdownItem>
-        <UserIcon />
-        <DropdownLabel> Foo</DropdownLabel>
-      </DropdownItem>
-      <DropdownItem
-        data-testid="user-sign-out"
-        color="red"
-        disabled={pending}
-        className="has-data-[slot=loading-icon]:bg-transparent! data-disabled:has-data-[slot=loading-icon]:opacity-100"
-        onClick={(e: React.MouseEvent) => {
-          e.preventDefault() // Prevent closing the dropdown menu. https://headlessui.com/react/menu#closing-menus-manually
-          startTransition(action)
-        }}
-      >
-        {pending ? (
-          <DropdownLabel className="flex h-6 items-center justify-self-center">
-            <LoadingSpinner className="size-4" />
-          </DropdownLabel>
-        ) : (
-          <DropdownLabel>Sign out</DropdownLabel>
-        )}
-      </DropdownItem>
-    </>
+    <DropdownItem
+      data-testid="user-sign-out"
+      color="red"
+      disabled={pending}
+      className="has-data-[slot=loading-icon]:bg-transparent! data-disabled:has-data-[slot=loading-icon]:opacity-100"
+      onClick={(e: React.MouseEvent) => {
+        e.preventDefault() // Prevent closing the dropdown menu. https://headlessui.com/react/menu#closing-menus-manually
+        startTransition(action)
+      }}
+    >
+      {pending ? (
+        <DropdownLabel className="flex h-6 items-center justify-self-center">
+          <LoadingSpinner className="size-4" />
+        </DropdownLabel>
+      ) : (
+        <DropdownLabel>Sign out</DropdownLabel>
+      )}
+    </DropdownItem>
   )
 }
